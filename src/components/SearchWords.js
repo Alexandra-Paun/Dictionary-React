463fe49a0fd1b916150bb32567f47464d3b47bd8
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-07-04 18:28:14 +0300

    Add files via upload

---
+++ src/components/SearchWords.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function SearchWords({array}) {
+    const [word, setWord] = useState("");
+    const [message, setMessage] = useState("");
+
+    const add = () => {
+        if(array.includes(word)) {
+            setMessage("The word " + word + " exists in the dictionary!");  
+        } else {
+             setMessage("The word " + word + " doesn't exist in the dictionary!");
+        }
+    }
+
+   return (
+        <div >
+            <input 
+                type="text" 
+                value={word}
+                onChange={(e) => setWord(e.target.value)}
+            />
+            <button onClick = {add} >Search Word</button>
+            <p style={{color: "green"}}>{message}</p>
+        </div>
+    )
+}
+
+export default SearchWords;
