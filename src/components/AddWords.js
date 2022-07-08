6246d37bce61b8f37832fd174bed6aecb7c3d8ab
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-07-08 14:19:53 +0300

    Add files via upload

--- src/components/AddWords.js
+++ src/components/AddWords.js
@@ -1,24 +1,25 @@
 import { useState } from 'react';
 
-function SearchWords({ list }) {
+function AddWords({ list, setWordList }) {
   const [word, setWord] = useState('');
   const [message, setMessage] = useState('');
 
   const add = () => {
-    if (list.includes(word)) {
-      setMessage('The word ' + word + ' exists in the dictionary!');
+    if (!list.includes(word)) {
+      setWordList(list => [...list, word]);
+      setMessage('The word ' + word + ' has been added!');
     } else {
-      setMessage('The word ' + word + " doesn't exist in the dictionary!");
+      setMessage('The word ' + word + ' is already in the dictionary!');
     }
   };
 
   return (
     <div>
       <input type='text' value={word} onChange={e => setWord(e.target.value)} />
-      <button onClick={add}>Search Word</button>
+      <button onClick={add}>Add Word</button>
       <p style={{ color: 'green' }}>{message}</p>
     </div>
   );
 }
 
-export default SearchWords;
+export default AddWords;
