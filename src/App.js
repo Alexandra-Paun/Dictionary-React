f79c52c06655cd0e8168836b3032bf143cf31b67
Alexandra-Paun <101888869+Alexandra-Paun@users.noreply.github.com>
2022-07-08 14:18:57 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AddWords from './components/AddWords';
+import SearchWords from './components/SearchWords';
+import React, { useState } from 'react';
+
+function App() {
+  const [WordList, setWordList] = useState('');
+  return (
+    <div>
+      <h1>Dictionary</h1>
+      <SearchWords list={WordList} />
+      <AddWords list={WordList} setWordList={setWordList} />
+    </div>
+  );
+}
+export default App;
